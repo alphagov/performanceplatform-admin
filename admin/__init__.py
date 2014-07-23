505203c5915b4f1e4d85eb6f3bdfa1adc69bd94f
Jacob Ashdown <jcbashdown@gmail.com>
2014-07-23 17:26:46 +0100

    Add in assets and compilation from govuk_admin_template and bootstrap 3.1.0
    compilation uses pyscss by way of flask scss
    it should auto compile on start and first page load

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from os import getenv
+from os import getenv, path
 
 app = Flask(__name__)
+from flask.ext.scss import Scss
 
 GOVUK_ENV = getenv('GOVUK_ENV', 'development')
 
@@ -13,5 +14,12 @@ import admin.authentication
 
 
 def start(port):
-    app.debug = True
+    app.debug = app.config['DEBUG'] or False
+    if app.debug:
+        # does not watch for changes and recompile on requests if debug false
+        Scss(app,
+             static_dir='admin/static/css',
+             asset_dir='admin/assets/scss/manifest',
+             load_paths=[
+                 path.join(path.dirname(__file__), 'assets/scss')])
     app.run('0.0.0.0', port=port)
