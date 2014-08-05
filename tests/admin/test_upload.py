9e02a7510ff2b390a9631764d097488e0c4a30d5
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-08-05 12:34:46 +0100

    Fix second test

--- tests/admin/test_upload.py
+++ tests/admin/test_upload.py
@@ -7,44 +7,57 @@ from os import path
 
 from pprint import pprint
 
+
 class UploadTestCase(unittest.TestCase):
+
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    @patch('admin.files.uploaded.UploadedFile.is_virus')
     @patch('admin.helpers.get_context')
     @patch('admin.upload.get_data_set_config')
+    @patch('admin.files.uploaded.UploadedFile.is_virus')
     @patch('performanceplatform.client.data_set.DataSet.post')
     def test_user_can_post_to_upload_data(
             self,
             data_set_post_patch,
+            is_virus_patch,
             get_data_set_config_patch,
-            get_context_patch,
-            is_virus_patch):
+            get_context_patch):
         is_virus_patch.return_value = False
         get_context_patch.return_value = {'user': {'email': 'test@example.com'}}
-        get_data_set_config_patch.return_value = {'bearer_token':'abc123', 'foo': 'bar'}
+        get_data_set_config_patch.return_value = {
+            'bearer_token': 'abc123', 'foo': 'bar'
+        }
 
-
+        post_data = {
+            'carers-allowance-volumetrics-file':
+                (StringIO('_timestamp,foo\n2014-08-05T00:00:00Z,40'),
+                    'MYSPECIALFILE.csv')
+        }
         response = self.app.post('/upload-data/carers-allowance/volumetrics',
-                                     data={'carers-allowance-volumetrics-file': (StringIO('_timestamp,foo\n2014-08-05T00:00:00Z,40'), 'MYSPECIALFILE.csv')})
+                                 data=post_data)
 
-        print response
-
-        data_set_post_patch.assert_called_once_with([{u'_timestamp': u'2014-08-05T00:00:00Z', u'foo': 40}])
+        expected_post = [{u'_timestamp': u'2014-08-05T00:00:00Z', u'foo': 40}]
+        data_set_post_patch.assert_called_once_with(expected_post)
 
         upload_done_path = '/upload-data/carers-allowance/volumetrics/done'
         assert_that(response.headers['Location'], ends_with(upload_done_path))
         assert_that(response.status_code, equal_to(302))
 
+    @patch('admin.helpers.get_context')
     @patch('admin.upload.get_data_set_config')
-    def test_no_data_set_config_returns_error(self, get_data_set_config_patch):
+    def test_no_data_set_config_returns_error(
+            self,
+            get_data_set_config_patch,
+            get_context_patch):
+        get_context_patch.return_value = {
+            'user': {'email': 'test@example.com'}}
         get_data_set_config_patch.return_value = None
         response = self.app.post('/upload-data/carers-allowance/volumetrics',
                                  data={'file': (StringIO('data'), 'file.xlsx')})
 
-        assert_that(response.status_code, equal_to(404))
+        assert_that(response.status_code, equal_to(403))
 
     def test_probem_in_spreadsheet_prevents_post_to_backdrop(self):
         assert_that(1, is_(2))
