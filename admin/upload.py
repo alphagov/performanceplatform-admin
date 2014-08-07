bdb0c8e8b8c6501243b7122a53e1d08f0a384f2b
Jacob Ashdown <jcbashdown@gmail.com>
2014-08-07 13:21:52 +0100

    pop session stuff, don't just read it - prevent multiple display

--- admin/upload.py
+++ admin/upload.py
@@ -12,7 +12,8 @@ from requests.exceptions import HTTPError
 @requires_authentication
 def upload_list_data_sets(session_context=None):
     if 'upload_okay_message' in session:
-        session_context['upload_okay_message'] = session['upload_okay_message']
+        session_context['upload_okay_message'] = session.pop(
+            'upload_okay_message')
     return render_template('data_sets.html', **session_context)
 
 
@@ -72,7 +73,7 @@ def build_http_flash(err, app_name):
 @app.route("/upload-data/<data_group>/<data_type>/error", methods=['GET'])
 @requires_authentication
 def upload_error(data_group, data_type, session_context=None):
-    problems = session['upload_problems']
+    problems = session.pop('upload_problems')
     session_context.update({
         'problems': problems,
         'data_set': {
