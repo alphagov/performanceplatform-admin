8c0e72be81e979042511fe64571da5f8ea463c33
Jacob Ashdown <jcbashdown@gmail.com>
2014-08-08 17:33:51 +0100

    Working group by group method

--- admin/helpers.py
+++ admin/helpers.py
@@ -3,6 +3,8 @@ from flask import session, redirect, url_for
 from functools import wraps
 from os import getenv
 from performanceplatform.client.admin import AdminAPI
+from itertools import groupby
+from operator import itemgetter
 
 
 environment = getenv('INFRASTRUCTURE_ENV', 'development')
@@ -36,3 +38,14 @@ def base_template_context():
 
 def signed_in(session):
     return 'oauth_user' in session and 'oauth_token' in session
+
+
+def group_by_group(data_sets):
+    grouped_data_sets = {}
+    for item in data_sets:
+        if item['data_group'] in grouped_data_sets:
+            grouped_data_sets[item['data_group']].append(item)
+        else:
+            grouped_data_sets[item['data_group']] = []
+            grouped_data_sets[item['data_group']].append(item)
+    return grouped_data_sets
