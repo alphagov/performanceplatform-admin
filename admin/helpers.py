72fd639bdd96424a087e72a0ca0b23155f21244b
Tom Booth <tombooth@gmail.com>
2014-08-08 16:17:52 +0100

    Use INFRASTRUCTURE_ENV for real env
    GOVUK_ENV only stores development/production in a rails style switch but
    we really want whether we are in preview, staging or production. This
    can be retrieved through the INFRASTRUCTURE_ENV variable.

--- admin/helpers.py
+++ admin/helpers.py
@@ -5,7 +5,7 @@ from os import getenv
 from performanceplatform.client.admin import AdminAPI
 
 
-GOVUK_ENV = getenv('GOVUK_ENV', 'development')
+environment = getenv('INFRASTRUCTURE_ENV', 'development')
 
 
 def requires_authentication(f):
@@ -28,8 +28,8 @@ def get_admin_client(session):
 def base_template_context():
     return {
         'environment': {
-            'name': GOVUK_ENV,
-            'human_name': GOVUK_ENV[:1].upper() + GOVUK_ENV[1:]
+            'name': environment,
+            'human_name': environment.capitalize()
         }
     }
 
