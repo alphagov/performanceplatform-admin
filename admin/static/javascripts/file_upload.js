9fbdd1a05228f81b8b3ec5b816c9303c658cedb1
Tom Booth <tombooth@gmail.com>
2014-08-15 16:11:15 +0100

    Improve file_upload CSS
    Catch a few different cases where message might not have a payload or
    the payload might not be a list.
    
    Wrap everything up in a closure because it is pleasant.

--- admin/static/javascripts/file_upload.js
+++ admin/static/javascripts/file_upload.js
@@ -1,32 +1,52 @@
-function setMessage(className) {
-	return function (file, message) {
-		var form = $(file.previewElement).parent(),
-		    messageElem = form.find('.dz-message'),
-		    messageList = document.createElement('ul');
+(function() {
 
-		this.disable();
+  function createListElement(text) {
+    var elem = document.createElement('li');
+    elem.textContent = text;
+    return elem;
+  }
 
-		for (var i = 0; i < message.payload.length; i++) {
-			listItem = document.createElement('li');
-			listItem.textContent = message.payload[i];
-			messageList.appendChild(listItem);
-		}
+  function setMessage(className) {
+    return function (file, message) {
+      var form = $(file.previewElement).parent(),
+          messageElem = form.find('.dz-message'),
+          messageList = document.createElement('ul'),
+          listItem;
 
-		messageElem.empty().append(messageList);
-		form.addClass(className);
-	}
-}
+      this.disable();
+
+      if (message.payload) {
+        if (message.payload.length) {
+          for (var i = 0; i < message.payload.length; i++) {
+            messageList.appendChild(
+              createListElement(message.payload[i]));
+          }
+        } else {
+            messageList.appendChild(
+              createListElement(message.payload));
+        }
+      } else {
+        messageList.appendChild(
+            createListElement(message));
+      }
+
+      messageElem.empty().append(messageList);
+      form.addClass(className);
+    }
+  }
 
 
-window.Dropzone.prototype.defaultOptions['maxFiles'] = 1;
-window.Dropzone.prototype.defaultOptions['previewTemplate'] = '<div></div>';
-window.Dropzone.prototype.defaultOptions['init'] = function() {
-	this.on("error", setMessage('error')); 
-	this.on("success", setMessage('success')); 
-	this.on("sending", function(file) {
-		$(file.previewElement)
-			.parent()
-			.find('.dz-message')
-			.text('Uploading ' + file.name + '...');
-	});
-}
+  window.Dropzone.prototype.defaultOptions['maxFiles'] = 1;
+  window.Dropzone.prototype.defaultOptions['previewTemplate'] = '<div></div>';
+  window.Dropzone.prototype.defaultOptions['init'] = function() {
+    this.on("error", setMessage('error'));
+    this.on("success", setMessage('success'));
+    this.on("sending", function(file) {
+      $(file.previewElement)
+        .parent()
+        .find('.dz-message')
+        .text('Uploading ' + file.name + '...');
+    });
+  }
+
+})();
