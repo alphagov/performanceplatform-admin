639028048b5a5757e22d0ea88073b54876f75afb
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-05 10:23:22 +0100

    [#73958524] Add a create dashboard route
    This page will contain a form to allow creating dashboards.

--- admin/dashboards.py
+++ admin/dashboards.py
@@ -7,7 +7,10 @@ from admin.helpers import (
 from flask import render_template, session
 
 
-@app.route('/administer-dashboards', methods=['GET'])
+DASHBOARD_ROUTE = '/administer-dashboards'
+
+
+@app.route('{0}'.format(DASHBOARD_ROUTE), methods=['GET'])
 @requires_authentication
 @requires_permission('dashboard')
 def dashboard_admin_index(admin_client):
@@ -16,3 +19,14 @@ def dashboard_admin_index(admin_client):
         'user': session['oauth_user'],
     })
     return render_template('dashboards/index.html', **template_context)
+
+
+@app.route('{0}/create'.format(DASHBOARD_ROUTE), methods=['GET'])
+@requires_authentication
+@requires_permission('dashboard')
+def dashboard_admin_create(admin_client):
+    template_context = base_template_context()
+    template_context.update({
+        'user': session['oauth_user'],
+    })
+    return render_template('dashboards/create.html', **template_context)
