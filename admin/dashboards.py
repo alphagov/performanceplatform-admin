7ec903f0c9f49fbab8c7c76a7e84a0d2ee22af3f
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-10 12:36:51 +0100

    Implement dashboard creation form
    This commit introduces a form that will POST to Stagecraft to
    create a dashboard.
    
    It makes use of the session to store the form if it fails validation,
    and then presents it back to the user.

--- admin/dashboards.py
+++ admin/dashboards.py
@@ -1,10 +1,17 @@
 from admin import app
+from admin.forms import DashboardCreationForm
 from admin.helpers import (
     base_template_context,
     requires_authentication,
     requires_permission,
 )
-from flask import render_template, session
+from flask import (
+    flash, redirect, render_template, request,
+    session, url_for
+)
+
+import json
+import requests
 
 
 DASHBOARD_ROUTE = '/administer-dashboards'
@@ -29,4 +36,59 @@ def dashboard_admin_create(admin_client):
     template_context.update({
         'user': session['oauth_user'],
     })
-    return render_template('dashboards/create.html', **template_context)
+
+    if 'pending_dashboard' in session:
+        form = DashboardCreationForm(data=session['pending_dashboard'])
+    else:
+        form = DashboardCreationForm(request.form)
+
+    return render_template('dashboards/create.html',
+                           form=form,
+                           **template_context)
+
+
+@app.route('{0}/create'.format(DASHBOARD_ROUTE), methods=['POST'])
+@requires_authentication
+@requires_permission('dashboard')
+def dashboard_admin_create_post(admin_client):
+    form = DashboardCreationForm(request.form)
+
+    access_token = session['oauth_token']['access_token']
+    dashboard_url = "{0}/dashboard".format(app.config['STAGECRAFT_HOST'])
+    data = {
+        'published': False,
+        'page-type': 'dashboard',
+        'dashboard-type': form.dashboard_type.data,
+        'slug': form.slug.data,
+        'title': form.title.data,
+        'description': form.description.data,
+        'customer_type': form.customer_type.data,
+        'business_model': form.business_model.data,
+        'strapline': form.strapline.data,
+        'links': [{
+            'title': form.transaction_title.data,
+            'url': form.transaction_link.data,
+            'type': 'transaction',
+        }]
+    }
+    headers = {
+        'Authorization': 'Bearer {0}'.format(access_token),
+        'Content-type': 'application/json',
+    }
+
+    create_dashboard = requests.post(dashboard_url,
+                                     data=json.dumps(data),
+                                     headers=headers)
+
+    if create_dashboard.status_code == 200:
+        if 'pending_dashboard' in session:
+            del session['pending_dashboard']
+        flash('Created the {0} dashboard'.format(form.slug.data), 'success')
+        return redirect(url_for('dashboard_admin_index'))
+    else:
+        session['pending_dashboard'] = request.form
+        stagecraft_message = create_dashboard.json()['message']
+        formatted_error = 'Error creating the {0} dashboard: {1}'.format(
+            form.slug.data, stagecraft_message)
+        flash(formatted_error, 'danger')
+        return redirect(url_for('dashboard_admin_create'))
---
+++ tests/admin/test_dashboards.py
@@ -0,0 +1,98 @@
+from tests.admin.support.flask_app_test_case import FlaskAppTestCase
+from admin import app
+from hamcrest import assert_that, contains_string, ends_with, equal_to
+from mock import patch, Mock
+
+import json
+import requests
+
+
+class DashboardTestCase(FlaskAppTestCase):
+    def setUp(self):
+        app.config['WTF_CSRF_ENABLED'] = False
+        self.app = app.test_client()
+
+    def test_create_form_uses_pending_dashboard_if_stored(self):
+        with self.app as admin_app:
+            with admin_app.session_transaction() as session:
+                session['oauth_token'] = {'access_token': 'token'}
+                session['oauth_user'] = {
+                    'permissions': ['signin', 'dashboard']
+                }
+                session['pending_dashboard'] = {'slug': 'my-valid-slug'}
+
+            resp = admin_app.get('/administer-dashboards/create')
+
+            assert_that(resp.data, contains_string('value="my-valid-slug"'))
+
+    @patch("requests.post")
+    def test_create_post_sends_a_post_to_stagecraft(self, post_patch):
+        successful_post = requests.Response()
+        successful_post.status_code = 200
+        post_patch.return_value = successful_post
+
+        with self.app as admin_app:
+            with admin_app.session_transaction() as session:
+                session['oauth_token'] = {'access_token': 'token'}
+                session['oauth_user'] = {
+                    'permissions': ['signin', 'dashboard']
+                }
+
+            resp = admin_app.post('/administer-dashboards/create',
+                                  data={'slug': 'valid-slug'})
+
+        post_headers = {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer token',
+        }
+        post_json = json.loads(post_patch.call_args[1]['data'])
+
+        assert_that(post_patch.call_args[0][0], ends_with('/dashboard'))
+        assert_that(post_patch.call_args[1]['headers'], equal_to(post_headers))
+        assert_that(post_json['slug'], equal_to('valid-slug'))
+        assert_that(resp.status_code, equal_to(302))
+
+    @patch("requests.post")
+    def test_creating_dashboard_deletes_from_session(self, post_patch):
+        successful_post = requests.Response()
+        successful_post.status_code = 200
+        post_patch.return_value = successful_post
+
+        dashboard_data = {
+            'slug': 'valid-slug',
+        }
+
+        with self.client.session_transaction() as session:
+            session['oauth_token'] = {'access_token': 'token'}
+            session['oauth_user'] = {
+                'permissions': ['signin', 'dashboard']
+            }
+            session['pending_dashboard'] = dashboard_data
+
+        resp = self.client.post('/administer-dashboards/create',
+                                data=dashboard_data)
+
+        self.assert_session_does_not_contain('pending_dashboard')
+        self.assert_flashes('Created the valid-slug dashboard', 'success')
+
+    @patch("requests.post")
+    def test_failed_dashboard_creation_stored_in_session(self, post_patch):
+        response_json_mock = Mock()
+        response_json_mock.return_value = {'message': 'Error message'}
+        successful_post = requests.Response()
+        successful_post.status_code = 400
+        successful_post.json = response_json_mock
+        post_patch.return_value = successful_post
+
+        with self.client.session_transaction() as session:
+            session['oauth_token'] = {'access_token': 'token'}
+            session['oauth_user'] = {
+                'permissions': ['signin', 'dashboard']
+            }
+
+        resp = self.client.post('/administer-dashboards/create',
+                                data={'slug': 'foo'})
+
+        self.assert_session_contains('pending_dashboard', {'slug': 'foo'})
+        self.assert_flashes('Error creating the foo dashboard: Error message',
+                            'danger')
