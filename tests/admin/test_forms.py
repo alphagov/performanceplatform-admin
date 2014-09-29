d130430aceed72674cfcf06bf332ec192534f352
Jacob Ashdown <jcbashdown@gmail.com>
2014-09-29 12:42:51 +0100

    wip possible better way to test forms

--- tests/admin/test_forms.py
+++ tests/admin/test_forms.py
@@ -3,6 +3,7 @@ from admin.forms import convert_to_dashboard_form
 from hamcrest import assert_that, equal_to
 import os
 import json
+from admin.dashboards import build_dict_for_post
 
 
 class DashboardTestCase(TestCase):
@@ -13,7 +14,12 @@ class DashboardTestCase(TestCase):
             dashboard_json = file.read()
         dashboard_dict = json.loads(dashboard_json)
         dashboard_form = convert_to_dashboard_form(dashboard_dict)
+        # stuff
+        dict_for_post = build_dict_for_post(form)
+        # stuff
         assert_that(dashboard_form.description.data, equal_to(dashboard_dict['description']))
         assert_that(dashboard_form.dashboard_type.data, equal_to(dashboard_dict['description']))
+        assert_that(dict_for_post.description, equal_to(dashboard_dict['description']))
+        assert_that(dict_for_post.dashboard_type, equal_to(dashboard_dict['description']))
 
 
