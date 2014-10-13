2a7a8df7501cb5339b10e2bcd418bec22c58d48f
Tim Mower <timothy.mower@gmail.com>
2014-10-13 15:39:44 +0100

    Use an existing virtualenv if it exists
    This is useful if you use virtualenv wrapper and are currently in a
    virtualenv with the requirements installed when you run start_app.sh

--- start-app.sh
+++ start-app.sh
@@ -2,12 +2,14 @@
 
 VENV_DIR=~/.virtualenvs/$(basename $(cd $(dirname $0) && pwd -P))-$1-$2
 
-if [ ! -f "${VENV_DIR}/bin/activate" ]; then
+if [ -z "$VIRTUAL_ENV" ]; then
+  if [ ! -f "${VENV_DIR}/bin/activate" ]; then
     mkdir -p "${VENV_DIR}"
     virtualenv --no-site-packages "$VENV_DIR"
-fi
+  fi
 
-source "$VENV_DIR/bin/activate"
+  source "$VENV_DIR/bin/activate"
+fi
 
 echo "Installing dependencies"
 pip install -r requirements.txt
