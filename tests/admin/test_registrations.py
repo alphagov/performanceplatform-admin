b09024bcfbe3722c3cf307779482cdd07693a025
Lee Longmore <lee.longmore@gmail.com>
2015-03-02 14:13:59 +0000

    Send dashboard application email

--- tests/admin/test_registrations.py
+++ tests/admin/test_registrations.py
@@ -83,11 +83,16 @@ class AboutYouPageTestCase(FlaskAppTestCase):
                     ends_with('/about-your-service'))
 
 
+@patch("boto.ses.connect_to_region")
 class AboutYourServicePageTestCase(FlaskAppTestCase):
 
     def setUp(self):
         self.app = app.test_client()
         app.config['WTF_CSRF_ENABLED'] = False
+        with self.client.session_transaction() as session:
+            session['full_name'] = 'Foo Bar'
+            session['email_address'] = 'foo@example.com'
+            session['organisation_name'] = 'Mock organisation'
 
     @staticmethod
     def params(options={}):
@@ -99,40 +104,64 @@ class AboutYourServicePageTestCase(FlaskAppTestCase):
         params.update(options)
         return params
 
-    def test_service_name_field_is_required(self):
+    def test_service_name_field_is_required(self, mock_ses_connection):
         data = self.params({'service_name': ''})
-        response = self.app.post('/register/about-your-service', data=data)
+        response = self.client.post('/register/about-your-service', data=data)
         assert_that(response.status, equal_to('200 OK'))
         assert_that(response.data, contains_string('Name cannot be blank'))
 
-    def test_service_url_field_is_optional(self):
+    def test_service_url_field_is_optional(self, mock_ses_connection):
         data = self.params()
-        response = self.app.post('/register/about-your-service', data=data)
+        response = self.client.post('/register/about-your-service', data=data)
         assert_that(response.status, equal_to('302 FOUND'))
 
-    def test_service_url_is_like_a_url(self):
+    def test_service_url_is_like_a_url(self, mock_ses_connection):
         data = self.params({'service_url': 'www.foo..com'})
-        response = self.app.post('/register/about-your-service', data=data)
+        response = self.client.post('/register/about-your-service', data=data)
         assert_that(response.status, equal_to('200 OK'))
         assert_that(response.data,
                     contains_string('Start page URL format is invalid'))
 
-    def test_service_description_field_is_required(self):
+    def test_service_description_field_is_required(self, mock_ses_connection):
         data = self.params({'service_description': ''})
-        response = self.app.post('/register/about-your-service', data=data)
+        response = self.client.post('/register/about-your-service', data=data)
         assert_that(response.status, equal_to('200 OK'))
         assert_that(response.data,
                     contains_string('Service description cannot be blank'))
 
-    def test_stores_submitted_data_in_the_session(self):
+    def test_stores_submitted_data_in_the_session(self, mock_ses_connection):
         self.client.post('/register/about-your-service', data=self.params())
         self.assert_session_contains('service_name', 'Foo')
         self.assert_session_contains('service_url', '')
         self.assert_session_contains('service_description', 'All about foo')
 
-    def test_redirects_to_confirmation_page(self):
+    def test_sends_details_of_the_application_by_email(
+            self, mock_ses_connection):
+        data = self.params({'service_url': 'http://www.foo.com'})
+        self.client.post('/register/about-your-service', data=data)
+        second_call_args = mock_ses_connection.mock_calls[1][1]
+        assert_that(second_call_args[0],
+                    equal_to(app.config['NO_REPLY_EMAIL']))
+        assert_that(second_call_args[1], equal_to('New dashboard application'))
+        assert_that(second_call_args[2],
+                    contains_string('Full name: Foo Bar\n'))
+        assert_that(second_call_args[2],
+                    contains_string('Email address: foo@example.com\n'))
+        assert_that(second_call_args[2],
+                    contains_string('Organisation name: Mock organisation\n'))
+        assert_that(second_call_args[2],
+                    contains_string('Service name: Foo\n'))
+        assert_that(second_call_args[2],
+                    contains_string('Service URL: http://www.foo.com\n'))
+        assert_that(second_call_args[2],
+                    contains_string('Service description:\nAll about foo'))
+        assert_that(
+            second_call_args[3],
+            equal_to(app.config['NOTIFICATIONS_EMAIL']))
+
+    def test_redirects_to_confirmation_page(self, mock_ses_connection):
         data = self.params()
-        response = self.app.post('/register/about-your-service', data=data)
+        response = self.client.post('/register/about-your-service', data=data)
         assert_that(response.status, equal_to('302 FOUND'))
         assert_that(response.headers['Location'],
                     ends_with('/confirmation'))
