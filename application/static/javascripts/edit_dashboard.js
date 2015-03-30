ebb2557c40577ab449ca056ab0bae6cf9a24e906
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-03-30 11:02:32 +0100

    In-line JSON validation for query parameters and visualisation settings fields.

---
+++ application/static/javascripts/edit_dashboard.js
@@ -0,0 +1,21 @@
+(function () {
+
+  function setupJsonValidation($textareas) {
+    $textareas.each(function() {
+      $(this).on('keyup', function () {
+        $(this).removeClass('invalid').next('.msg-invalid-json').remove();
+        try {
+          jsonlint.parse($(this).val());
+        } catch (err) {
+          $(this).addClass('invalid').after('<div class="msg-invalid-json">Invalid JSON: ' + err + '</div>');
+          $(this).linedtextarea(
+            {selectedLine: 1}
+          );
+        }
+      });
+    });
+  }
+
+  setupJsonValidation($('.json-field'));
+
+}());
